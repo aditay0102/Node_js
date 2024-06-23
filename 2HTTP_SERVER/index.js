43389036628990af340adefcaf73650785f9acdf
unknown <aditya0102.be21@chitkara.edu.in>
2024-06-23 18:32:30 +0530

    added another thig

---
+++ 2HTTP_SERVER/index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const fs = require("fs");
+
+
+const myServer  = http.createServer((req,res) => {
+    const log =    `${Date.now()}: New Req Received \n`;
+
+    fs.appendFile("log.txt",log,(err,data) =>{
+        res.end("Hello From Server");
+        
+    })
+
+
+    
+});
+
+myServer.listen(8000, () => console.log("sever started"));
+
